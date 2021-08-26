56f1d88a59758cd2de49e9efa0859b357715f028
nathan.hsieh <yi82121@gmail.com>
2021-08-26 16:33:21 +0800

    feat: add Static for experiment data recording

---
+++ main/java/data/Static.java
@@ -0,0 +1,26 @@
+package data;
+
+public class Static {
+    private static int successCase = 0;
+    private static int failCase = 0;
+
+    public synchronized static void incrementSuccess() {
+        successCase ++;
+    }
+
+    public synchronized static void incrementFail() {
+        failCase ++;
+    }
+
+    public synchronized static int recordAndClearSuccess() {
+        int result = successCase;
+        successCase = 0;
+        return result;
+    }
+
+    public synchronized static int recordAndClearFail() {
+        int result = failCase;
+        failCase = 0;
+        return result;
+    }
+}
